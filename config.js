ee16923f629d32f0e87adba4446972ea60deb29b
Eric Joshua Castor <70999862+Xastrophe@users.noreply.github.com>
2026-02-11 14:41:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://youtu.be/B4HD6aC4Wos", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/duoidiyqu/video/upload/v1770792005/Make_It_With_You_-_Ben_Ben_Lyrics_vguia8.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
